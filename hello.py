ebdd69ee525bb80aa57310c42aeb204c84855677
Bardodev05 <andersondev0526@gmail.com>
2024-04-08 05:00:53 -0500

    tipos de datos

--- hello.py
+++ hello.py
@@ -1,2 +1,25 @@
 #hola mundo
+#esto es un comentario
 print("hola mundo")
+print('hola python')
+
+
+
+'''
+este es un 
+comentario en 
+varias lineas
+'''
+
+"""
+este es un 
+comentario en 
+varias lineas
+"""
+
+#consulrar el tipo de dato
+print(type("hola mundo")) #tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(True)) #tipo 'bool'
+print(type(1 + 2j))#tipo de  'complex number'
