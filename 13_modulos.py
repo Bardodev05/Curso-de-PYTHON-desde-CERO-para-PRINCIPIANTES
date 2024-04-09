bc976e2460aba3dfe309c62d40e31d790097b0bb
Bardodev05 <andersondev0526@gmail.com>
2024-04-09 06:58:44 -0500

    modulos

---
+++ 13_modulos.py
@@ -0,0 +1,20 @@
+### Modules ###
+
+from math import pi as PI_VALUE
+import math
+from modulo import sumValue, printValue
+import modulo
+
+modulo.sumValue(5, 3, 1)
+modulo.printValue("Hola Python!")
+
+
+sumValue(5, 3, 1)
+printValue("Hola python")
+
+
+print(math.pi)
+print(math.pow(2, 8))
+
+
+print(PI_VALUE)
